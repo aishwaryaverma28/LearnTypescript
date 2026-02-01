be0696be047c8a91fddc3da28c72bc1435050b49
aishwaryaverma28 <aishwarya.verma28@gmail.com>
2026-02-01 18:11:46 +0530

    started

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const user = "TypeScript";
+console.log(greet(user));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+const user = "TypeScript";
+console.log(greet(user));
